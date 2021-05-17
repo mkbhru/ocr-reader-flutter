d600f268ed2c08ecb2599e30aafc852c27f8db0a
Prashant <prashant.th02@gmail.com>
2021-05-17 16:28:13 +0530

    Added Barcode

--- ocr_reader/lib/main.dart
+++ ocr_reader/lib/main.dart
@@ -21,9 +21,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'OCR Reader'),
     );
   }
 }
@@ -47,12 +47,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool isInitial = false;
+
+  void _scanBarcode() async {
+    List<Barcode> barcodes = [];
+    try {
+      barcodes = await FlutterMobileVision.scan(
+        waitTap: true,
+        showText: true,
+      );
+      if (barcodes.length > 0) {
+        for (Barcode barcode in barcodes) {
+          print(
+              'barcodevalueis ${barcode.displayValue} ${barcode.getFormatString()} ${barcode.getValueFormatString()}');
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // int _counter = 0;
+  bool isInitialized = false;
   @override
   void initState() {
-    bool isInitialized = false;
     FlutterMobileVision.start().then((value) {
       isInitialized = true;
+      setState(() {
+        isInitial = true;
+      });
     });
     super.initState();
   }
@@ -62,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
     try {
       list = await FlutterMobileVision.read(
         waitTap: true,
-        fps: 15.0,
+        fps: 10.0,
       );
       for (OcrText text in list) {
         print('valueis ${text.value}');
@@ -83,53 +106,48 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Card(
+        color: Colors.grey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome To OCR Reader',
+                style: TextStyle(fontSize: 30),
+              ),
+              //         floatingActionButton: FloatingActionButton(
+              //   onPressed: _startScan,
+              //   tooltip: 'Increment',
+              //   child: Icon(Icons.find_in_page),
+              // ),
+              ElevatedButton(
+                onPressed: _startScan,
+                child: Icon(Icons.find_in_page),
+              ),
+              // floatingActionButton: FloatingActionButton(
+              //   onPressed: _scanBarcode,
+              //   tooltip: 'Increment',
+              //   child: Icon(Icons.find_in_page),
+              // ),
+              ElevatedButton(
+                onPressed: _scanBarcode,
+                child: Icon(Icons.edit),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _startScan,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _app,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.find_in_page),
+      // ),
     );
   }
 }
